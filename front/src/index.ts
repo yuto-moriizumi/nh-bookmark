3ac39e912a2065f2ca15176a70c5aa4cf22b9ea3
volgakurvar <kurvan1112@gmail.com>
2024-05-19 09:26:35 +0900

    Fixed order to be consistent regardless of updates

--- front/src/index.ts
+++ front/src/index.ts
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { Root } from "./component";
 
-const menu = document.getElementsByTagName("ul").item(0);
-const rootDom = document.createElement("li");
-menu?.appendChild(rootDom);
-createRoot(rootDom).render(Root());
+// skip execution in the loading screen
+if (!document.getElementById("challenge-running")) {
+  const menu = document.getElementsByTagName("ul").item(0);
+  const rootDom = document.createElement("li");
+  menu?.appendChild(rootDom);
+  createRoot(rootDom).render(Root());
+}
