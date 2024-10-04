e25b324ef5d2320fff53e99a4e8a6904001545a5
volgakurvar <kurvan1112@gmail.com>
2024-10-04 23:57:33 +0900

    型名にコメントを追加

--- front/src/types.ts
+++ front/src/types.ts
@@ -5,10 +5,12 @@ export type OptionalSubscription = Partial<Subscription> &
 export type Subscription = {
   sub_url: string;
   work_url: string;
+  /** 最新の作品名 */
   title: string;
   image: string;
   updated_at: number;
   checked_at: number;
+  /** 作者名 */
   name: string;
   rank: number;
   has_new: boolean;
